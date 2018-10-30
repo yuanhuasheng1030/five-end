3ba837d30e97e1b63fe4fd8d55639b2275838641
Chen <2669204393@qq.com>
2018-10-30 10:22:02 +0800

    新增服务管理

--- src/components/manage/beforeStage/ServiceManage/serviceManage.vue
+++ src/components/manage/beforeStage/ServiceManage/serviceManage.vue
@@ -1,16 +1,98 @@
 <template>
-  <div>
-    服务管理
+<div>
+  <div class="searchAndAdd">
+    <Search class="search"></Search>
+    <Add></Add>
   </div>
+  <Update></Update>
+  <el-table
+    :data="services"
+    >
+    <el-table-column
+      label="类别"
+      width="100">
+      <template slot-scope="scope">
+        <span>{{ scope.row.serviceType }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="名称"
+      width="100">
+      <template slot-scope="scope">
+        <span>{{ scope.row.name }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="售价"
+      width="100">
+      <template slot-scope="scope">
+        <span>{{ scope.row.basePrice }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="会员价"
+      width="100">
+      <template slot-scope="scope">
+        <span>{{ scope.row.VIPprice }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="可预约时间"
+      width="200">
+      <template slot-scope="scope">
+        <span>{{ scope.row.schedule }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <Page></Page>
+</div>
 </template>
 
 <script>
+import Search from "./Search.vue";
+import Page from "./Page.vue";
+import Add from "./Add.vue";
+import Update from "./Update.vue";
+import { mapState, mapActions, mapMutations } from "vuex";
+import axios from 'axios';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function() {
+    return {
+    };
+  },
+  components: { Search, Page, Add, Update },
+  created() {
+    this.setServices();
+  },
+  computed: {
+    ...mapState(["services", "pagination"])
+  },
+  methods: {
+    ...mapActions(["setServices","setService","updateService"]),
+    ...mapMutations(["setDialogUpdateVisible"]),
+    handleDelete(index, row) {
+      this.id=row._id;
+      axios.delete("/services/" + row._id, {}).then(response => {
+        this.setServices();
+      });
+    },
+    handleEdit(index, row) {
+      this.setService(row._id);
+      this.setDialogUpdateVisible(true);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -28,5 +110,13 @@ li {
 }
 a {
   color: #42b983;
+}
+.searchAndAdd {
+  display: flex;
+  margin-bottom: 30px;
+}
+.search {
+  width: 400px;
+  margin-right: 30px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,94 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    services: [],
+    service: {},
+    pagination: {},
+    search: {},
+    dialogUpdateVisible: false,
+    username: "张雨霖",
+    shopName: "爱宠小店"
   },
   mutations: {
-
+    setServices(state, services) {
+      state.services = services
+    },
+    setPagination(state, pagination) {
+      state.pagination = pagination
+    },
+    setSearch(state, search) {
+      state.search = search
+    },
+    setService(state, service) {
+      state.service = service
+    },
+    setDialogUpdateVisible(state, upload) {
+      state.dialogUpdateVisible = upload
+    }
   },
   actions: {
+    setServices({
+      commit
+    }, payload = {}) {
+      axios.get('/services', {
+        params: {
+          name: payload.name || '',
+          value: payload.value || '',
+          username: this.state.username,
+          shopName: this.state.shopName,
+          page: payload.page || 1,
+          rows: payload.rows || 3
+        }
+      }).then((response) => {
 
+        commit('setServices', response.data.rows)
+        commit('setPagination', response.data)
+      })
+    },
+    addService({
+      dispatch
+    }, payload = {}) {
+      axios.post('/services', {
+        newService: { ...payload,
+          username: this.state.username,
+          shopName: this.state.shopName
+        }
+      }).then(() => {
+        dispatch('setServices');
+      })
+    },
+    setService({
+      commit
+    }, id) {
+      axios.get('/services/' + id, {}).then((response) => {
+        commit('setService', response.data)
+      })
+    },
+    updateService({
+      dispatch
+    }, payload = {}) {
+      axios({
+        method: "put",
+        url: "/services/" + this.state.service._id,
+        data: {
+          name: this.state.service.name,
+          serviceType: this.state.service.serviceType,
+          basePrice: this.state.service.basePrice,
+          VIPprice: this.state.service.VIPprice,
+          serviceStandard: this.state.service.serviceStandard,
+          timeCost: this.state.service.timeCost,
+          adaptPets: this.state.service.adaptPets,
+          username: this.state.username,
+          shopName: this.state.shopName,
+          startTime: this.state.service.startTime,
+          endTime: this.state.service.endTime,
+          schedule: `${this.state.service.startTime}-${this.state.service.endTime}`
+        }
+      })
+    }
   }
 })
